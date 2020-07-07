7a9f52d028fb98b19afa8eecb5fca6b0ccb5fd99
geoffrey45 <geoffreymungai45@gmail.com>
2020-07-07 11:04:33 +0300

    add a basic 404 page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,8 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgProgressModule } from '@ngx-progressbar/core'
 import { NgProgressHttpClientModule } from '@ngx-progressbar/http-client';
 import { AboutComponent } from './about/about.component';
-import { NavbarComponent } from './navbar/navbar.component'
+import { NavbarComponent } from './navbar/navbar.component';
+import { NotFoundComponent } from './not-found/not-found.component'
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import { NavbarComponent } from './navbar/navbar.component'
     DateCountPipe,
     GoalFormComponent,
     AboutComponent,
-    NavbarComponent
+    NavbarComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
